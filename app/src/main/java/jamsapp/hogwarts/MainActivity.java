dff9ee369d03cde19c70dd351f4d48189565dc05
SanmiguelLabco <c.sanmiguel@labco.com.co>
2017-04-12 18:46:04 -0500

    Se agregaron todas las vistas  conectadas menos la de cambiarcontraseña con modificarusu

--- app/src/main/java/jamsapp/hogwarts/MainActivity.java
+++ app/src/main/java/jamsapp/hogwarts/MainActivity.java
@@ -1,13 +1,38 @@
 package jamsapp.hogwarts;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+     Button boton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        boton = (Button) findViewById(R.id.button);
+
+
+        boton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+
+                        Intent i = new Intent(MainActivity.this, Menu.class);
+                        String cadena = "Bienvenido [NOMBRE]";
+                        Toast notificacion = Toast.makeText(getApplicationContext(), cadena, Toast.LENGTH_LONG);
+                        notificacion.show();
+                        startActivity(i);
+
+
+                    }});
+
+
     }
 }
---
+++ app/src/main/java/jamsapp/hogwarts/Menu.java
@@ -0,0 +1,78 @@
+package jamsapp.hogwarts;
+
+import android.content.Intent;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class Menu extends AppCompatActivity {
+ImageView imagen1, imagen2, imagen3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        imagen1= (ImageView) findViewById(R.id.imageView4);
+        imagen2= (ImageView) findViewById(R.id.imageView5);
+        imagen3= (ImageView) findViewById(R.id.imageView7);
+
+        imagen1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+
+                        Intent i = new Intent(Menu.this,ModificarUSU.class);
+                        String cadena = "Bienvenido [NOMBRE]";
+                        Toast notificacion = Toast.makeText(getApplicationContext(), cadena, Toast.LENGTH_LONG);
+                        notificacion.show();
+                        startActivity(i);
+
+
+                    }});
+        imagen2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+
+                        Intent i = new Intent(Menu.this,Matriculamateria.class);
+                        String cadena = "Bienvenido [NOMBRE]";
+                        Toast notificacion = Toast.makeText(getApplicationContext(), cadena, Toast.LENGTH_LONG);
+                        notificacion.show();
+                        startActivity(i);
+
+
+                    }});
+        imagen3.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+
+                        Intent i = new Intent(Menu.this,Eliminarmateria.class);
+                        String cadena = "Bienvenido [NOMBRE]";
+                        Toast notificacion = Toast.makeText(getApplicationContext(), cadena, Toast.LENGTH_LONG);
+                        notificacion.show();
+                        startActivity(i);
+
+
+                    }});
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
